6f342b6d41387f8f7ec08638141497bc89ec633d
ElSacho <sacha.braun99@gmail.com>
2023-12-25 15:56:20 +0100

    configs_working

--- trainer.py
+++ trainer.py
@@ -1,5 +1,5 @@
 from function_train import train
 
-list_config_path = ['attentionOnGraph/configs.json', 'attentionOnGraph/configs02.json']
+list_config_path = ['configs/oneHead_linear_NLP.json', 'configs/oneHead.json']
 
 train(list_config_path)
