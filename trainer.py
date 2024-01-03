0d0878fa764e5e53031f297174abdc1195191b40
ElSacho <sacha.braun99@gmail.com>
2024-01-03 09:58:32 +0100

    logs

--- trainer.py
+++ trainer.py
@@ -1,6 +1,7 @@
 from function_train import train
 
 # list_config_path = ['configs/oneHead_linear_NLP.json', 'configs/oneHead.json', 'configs/baseline.json', 'configs/initVQ.json', 'configs/noMLP.json']
-list_config_path = ['configs/3head_cheb_fine_tune.json', 'configs/3head_conv_fine_tune.json','configs/3head_conv_fine_tune_VQ.json' ]
-
+# list_config_path = ['configs/3head_cheb_fine_tune.json', 'configs/3head_conv_fine_tune.json','configs/3head_conv_fine_tune_VQ.json' ]
+# list_config_path = ['configs/3head_conv_fine_tune_VQ.json' ]
+list_config_path = ['configs/1head_conv_fine_tune_VQ_simplified.json', 'configs/1head_conv_fine_tune_VQ.json','configs/1head_conv_fine_tune_VQ_simplified_sci.json' ]
 train(list_config_path)
